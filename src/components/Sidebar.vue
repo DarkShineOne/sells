cc16e9c24041de622053caafea55aa3bf2840725
AnriTool <59344539+AnriTool@users.noreply.github.com>
2023-06-21 14:40:07 +1000

    Sidebar_retext

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -34,7 +34,7 @@ export default{
     z-index: 10;
     background-color:#fff;
     border-radius:8px;
-    box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.1);
+    box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.16);
   }
   .sidebar_search{
     margin-top: 0px;
