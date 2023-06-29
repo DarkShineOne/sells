15eb5421c0a404dc98698257f24d53523fecc0e1
Дмитрий Плутицкий <phoenixo_o@mail.ru>
2023-06-29 14:54:05 +1000

    some refactor

--- server/models/query-creator.js
+++ server/models/query-creator.js
@@ -9,6 +9,16 @@ exports.pageQuery= (params)=>{
     limit 18 offset 18*(${params.pageParam - 1});`
 }
 
+exports.pageCountQuery= (params)=>{
+    return `select count(*) from (select item.id, item.name, item.price, item.rating, item.discount, item.disctype, item.pricewithdiscount, string_agg(ItemToCharacteristic.value,', '),
+    item.logourl, item.itemlink, item.itemcount
+    from item
+    join ItemToCharacteristic on (ItemToCharacteristic.itemid = item.id)
+    join Characteristic on (Characteristic.id = ItemToCharacteristic.characteristicid)
+    where item.categoryid ${params.categoryParam} ${params.priceParam} group by item.name, item.price, item.id,
+    item.rating,item.logourl, item.itemlink order by ${params.sortParam[0]} ${params.sortParam[1]}) as s;`
+}
+
 exports.categoryQuery = ()=>{
     return `select * from category`
 }
